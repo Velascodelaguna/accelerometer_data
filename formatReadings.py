5146c06d7bce3372740524bd948bd62040c94757
James Velasco <jvelasco@sfu.ca>
2012-11-07 18:22:29 -0800

    Changed time format so that it takes in the change of time instead of actual time in seconds

--- formatReadings.py
+++ formatReadings.py
@@ -14,8 +14,22 @@ def get_time(time):
 	time_sec = time[last_colon + 1 :]
 	return time_sec
 
+# Only works if time is in ascending order
+def subtract_time(seconds_list):
+	delta = [0]
+	for i in range(1,len(seconds_list)):
+		t1 = seconds_list[i-1]
+		t2 = seconds_list[i]
+		if t1 > t2:
+			delta.append(round(delta[i-1] + 60 + t2 - t1, 12))
+		else:
+			delta.append(round(delta[i-1] + t2 - t1, 12))
+	return delta
+
 def get_mean(num_list):
-	return math.fsum(num_list)/len(num_list)
+	mean = math.fsum(num_list)/len(num_list)
+	round_mean = round(mean, 10)
+	return round_mean
 
 def get_vector_sum(x, y, z):
 	return math.sqrt((x * x) + (y * y) + (z * z))
@@ -24,13 +38,16 @@ def std_dev(num_list, mean):
 	total_val_minus_mean_squared = 0
 	for num in num_list:
 		total_val_minus_mean_squared = total_val_minus_mean_squared + math.pow(num - mean,2)
-	return math.sqrt(total_val_minus_mean_squared / len(num_list))
+	stddev_val = math.sqrt(total_val_minus_mean_squared / len(num_list))
+	round_stddev_val = round(stddev_val, 10)
+	return round_stddev_val
 
 
 print("Analyzing accelerometer data")
-file_name = input('Enter file name: ')
-path = "/Users/james/Dropbox/SFU Courses/CMPT 275/accelerometer data/" + file_name
+accel_readings_file_name = input('Enter file name: ')
+path = "/Users/james/Dropbox/SFU Courses/CMPT 275/accelerometer data/" + accel_readings_file_name
 accel_Data = open(path)
+
 x_lst, y_lst, z_lst, vector_sum = [],[],[],[]
 seconds, duration = [],[]
 date = ''
@@ -43,7 +60,8 @@ for line in accel_Data:
 		x_val = float(line_list[5].replace(',' , ''))
 		y_val = float(line_list[8].replace(',', ''))
 		z_val = float(line_list[11].replace('\n', ''))
-		vec_val = get_vector_sum(x_val,y_val,z_val)
+		vec_val = get_vector_sum(x_val, y_val, z_val)
+
 		date = line_list[0]
 		duration.append(line_list[1])
 		seconds.append(time_sec) 
@@ -52,6 +70,8 @@ for line in accel_Data:
 		y_lst.append(y_val)
 		z_lst.append(z_val)
 		vector_sum.append(vec_val)
+
+seconds = subtract_time(seconds)
 
 # Gather mean values of x, y, z
 mean_x = get_mean(x_lst)
@@ -64,9 +84,9 @@ min_y, max_y = min(y_lst), max(y_lst)
 min_z, max_z = min(z_lst), max(z_lst)
 
 # Gather the range values of x, y, z
-range_x = max_x - min_x
-range_y = max_y - min_y
-range_z = max_z - min_z
+range_x = round(max_x - min_x, 10)
+range_y = round(max_y - min_y, 10)
+range_z = round(max_z - min_z, 10)
 
 # Gather standard deviation values of x, y, z
 std_dev_x = std_dev(x_lst, mean_x)
@@ -91,14 +111,16 @@ count_file.truncate()
 
 out_file = open("/Users/james/Dropbox/SFU Courses/CMPT 275/accelerometer data/accelData" + count, "w")
 comment = input("Enter comment: ")
-out_file.write("% Date: {0}\n% Test Duration: {1} - {2}\n".format(date, duration[0], duration[len(duration)-1]))
+
+out_file.write("% Date: {0}\n% Test Duration: {1} - {2}\n".format(date, duration[0], duration[-1]))
+out_file.write("% " + accel_readings_file_name + "\n")
 out_file.write("% Comment:" + comment + "\n\n")
 
 out_file.write("% Statistics:\n")
-heading = "%{0:^25} {1:^25} {2:^25} {3:^25} {4:^25}\n".format('Mean', 'Min', 'Max', 'Range', 'Std.Dev')
-stat_format_x = "% X: {0: <25} {1: <25} {2: <25} {3: <25} {4: <25}\n".format(mean_x, min_x, max_x, range_x, std_dev_x)
-stat_format_y = "% Y: {0: <25} {1: <25} {2: <25} {3: <25} {4: <25}\n".format(mean_y, min_y, max_y, range_y, std_dev_y)
-stat_format_z = "% Z: {0: <25} {1: <25} {2: <25} {3: <25} {4: <25}\n\n".format(mean_z, min_z, max_z, range_z, std_dev_z)
+heading = "%{0:<17} {1:<12} {2:<12} {3:<12} {4:<12}\n".format('Mean', 'Min', 'Max', 'Range', 'Std.Dev')
+stat_format_x = "% X: {0: <15} {1: <10} {2: <10} {3: <10} {4: <15}\n".format(mean_x, min_x, max_x, range_x, std_dev_x)
+stat_format_y = "% Y: {0: <15} {1: <10} {2: <10} {3: <10} {4: <15}\n".format(mean_y, min_y, max_y, range_y, std_dev_y)
+stat_format_z = "% Z: {0: <15} {1: <10} {2: <10} {3: <10} {4: <15}\n\n".format(mean_z, min_z, max_z, range_z, std_dev_z)
 out_file.write(heading)
 out_file.write(stat_format_x)
 out_file.write(stat_format_y)
